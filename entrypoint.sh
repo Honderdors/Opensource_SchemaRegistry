3870003bc6423819f4e12a2ddd0ed55ee1875137
Raymond Honderdors <raymond@honderdors.com>
2019-04-22 19:16:10 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+/usr/lib/postgresql/9.6/bin/pg_ctl -w start -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf 
 mv /opt/hortonworks-registry/conf/registry-postgres-example.yaml /opt/hortonworks-registry/conf/registry.yaml
 /opt/hortonworks-registry/bootstrap/bootstrap-storage.sh drop 
 /opt/hortonworks-registry/bootstrap/bootstrap-storage.sh create 
